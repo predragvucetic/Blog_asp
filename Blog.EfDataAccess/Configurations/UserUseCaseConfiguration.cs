0e86d325ff98da2f40e1ca9f837c71ca696c7fee
predragvucetic <pedja.dota.vucetic>
2020-07-01 15:32:22 +0200

    My first Commit

---
+++ Blog.EfDataAccess/Configurations/UserUseCaseConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Blog.Domain;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.EfDataAccess.Configurations
+{
+    public class UserUseCaseConfiguration : IEntityTypeConfiguration<UserUseCase>
+    {
+        public void Configure(EntityTypeBuilder<UserUseCase> builder)
+        {
+            builder.HasKey(x => new { x.UserId, x.UseCaseId });
+        }
+    }
+}
---
+++ Blog.Implementation/Commands/EfCreateCommentCommand.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Blog.Application.Commands;
+using Blog.Application.DataTransfer;
+using Blog.Domain;
+using Blog.EfDataAccess;
+using Blog.Implementation.Validators;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Blog.Implementation.Commands
+{
+    public class EfCreateCommentCommand : ICreateCommentCommand
+    {
+        private readonly BlogContext _context;
+        private readonly CreateCommentValidator _validator;
+        private readonly IMapper _mapper;
+
+        public EfCreateCommentCommand(BlogContext context, CreateCommentValidator validator, IMapper mapper)
+        {
+            _context = context;
+            _validator = validator;
+            _mapper = mapper;
+        }
+
+        public int Id => 19;
+
+        public string Name => "Create New Comment";
+
+        public void Execute(CommentDto request)
+        {
+            _validator.ValidateAndThrow(request);
+
+            var comment = _mapper.Map<Comment>(request);
+
+            _context.Comments.Add(comment);
+
+            _context.SaveChanges();
+        }
+    }
+}
